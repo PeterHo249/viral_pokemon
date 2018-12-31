350e1136b946d851b192c51e14fb38ade07ab662
TayceDat <32897727+TayceDat@users.noreply.github.com>
2018-12-31 16:26:11 +0700

    up

--- Viral Pokemon/Assets/Scripts/Shop/ShopControl.cs
+++ Viral Pokemon/Assets/Scripts/Shop/ShopControl.cs
@@ -7,20 +7,22 @@ using UnityEngine.SceneManagement;
 public class ShopControl : MonoBehaviour
 {
     static int sumMoney;
-    static int maxAmount = 10;
-    static int amount;
+
+    public int priceMaxElixir;
+    public int priceMaxPotion;
 
     public Text moneyAmountText;
-    public Text AmoutItem;
-    public Text moneyItem;
-    public Button buyButton;
+
+    public Button buyButton1;
+    public Button buyButton2;
 
     // Start is called before the first frame update
     void Start()
     {
 
-        sumMoney = 1000000;
-        amount = 0;
+        sumMoney = 1000;
+        priceMaxElixir = 100;
+        priceMaxPotion = 200;
     }
 
     // Update is called once per frame
@@ -28,41 +30,56 @@ public class ShopControl : MonoBehaviour
     {
         moneyAmountText.text = Player.Money.ToString();
 
+        print(sumMoney);
+
         if (sumMoney >= 100)
-            buyButton.interactable = true;
+            buyButton1.interactable = true;
         else
-            buyButton.interactable = false;
+            buyButton1.interactable = false;
+
+        if (sumMoney >= 200)
+            buyButton2.interactable = true;
+        else
+            buyButton2.interactable = false;
     }
 
 
 
-    public void buyItem()
+    public void buyItem1()
     {
-        sumMoney -= 100*amount;
+        sumMoney -= 100;
+        print(sumMoney);
         moneyAmountText.text = sumMoney.ToString();
     }
 
-    public void Up()
+    public void buyItem2()
     {
-        if (amount < maxAmount)
-        {
-            amount += 1;
-            AmoutItem.text = amount.ToString();
-            moneyItem.text = (amount * 100).ToString();
-        }
-        if(amount * 100 < sumMoney)
-            buyButton.interactable = false;
+        sumMoney -= 200;
+        print(sumMoney);
+        moneyAmountText.text = sumMoney.ToString();
     }
 
-    public void Down()
-    {
-        if (amount > 0)
-        {
-            amount -= 1;
-            AmoutItem.text = amount.ToString();
-            moneyItem.text = (amount * 100).ToString();
-        }
-        if (amount * 100 < sumMoney)
-            buyButton.interactable = false;
-    }
+    //public void Up()
+    //{
+    //    if (amount < maxAmount)
+    //    {
+    //        amount += 1;
+    //        AmoutItem.text = amount.ToString();
+    //        moneyItem.text = (amount * 100).ToString();
+    //    }
+    //    if(amount * 100 < sumMoney)
+    //        buyButton.interactable = false;
+    //}
+
+    //public void Down()
+    //{
+    //    if (amount > 0)
+    //    {
+    //        amount -= 1;
+    //        AmoutItem.text = amount.ToString();
+    //        moneyItem.text = (amount * 100).ToString();
+    //    }
+    //    if (amount * 100 < sumMoney)
+    //        buyButton.interactable = false;
+    //}
 }
