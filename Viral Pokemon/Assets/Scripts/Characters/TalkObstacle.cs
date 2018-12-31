4890d953d866f9cdcc25a6a78c439d7e654868ad
TayceDat <32897727+TayceDat@users.noreply.github.com>
2018-12-31 11:29:17 +0700

    Fix Level

--- Viral Pokemon/Assets/Scripts/Characters/TalkObstacle.cs
+++ Viral Pokemon/Assets/Scripts/Characters/TalkObstacle.cs
@@ -6,18 +6,68 @@ using UnityEngine.UI;
 public class TalkObstacle : MonoBehaviour
 {
     public Text InputText;
+    public Text TalkText;
+    public GameObject Dialog;
+    public LevelManager levelManager;
 
+    public bool isCollise = false;
+
+
+    public string talk = "Press Space to talk";
     // Start is called before the first frame update
     void Start()
     {
+        levelManager = FindObjectOfType<LevelManager>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isCollise == true)
+        {
+            if (levelManager.state == true)
+            {
+                levelManager.state = false;
+                InputText.text = "";
+            }
+            else
+            {
+                levelManager.state = true;
+                InputText.text = "";
+            }
+        }
+
+        print(levelManager.state);
+        if (levelManager.state)
+        {
+            ShowElement();
+        }
+        else
+        {
+            HideElement();
+        }
+    }
+
+    public void ShowElement()
+    {
+        Dialog.SetActive(true);
+        TalkText.text = "123456789 123456789 123456789 123456789 123456789 123456789 123456789";
         
+    }
+
+    public void HideElement()
+    {
+        Dialog.SetActive(false);
+        TalkText.text = "";
+        //InputText.text = "";
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            InputText.text = ("Press Space to talk");
+            isCollise = true;
+            print("Va cham nhan vat");
+            InputText.text = (talk);
         }
     }
 
@@ -25,10 +75,7 @@ public class TalkObstacle : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
-            if (Input.GetKey(KeyCode.Space))
-            {
-                //Màn hình nói chuyện
-            }
+           
         }
     }
 
@@ -36,6 +83,8 @@ public class TalkObstacle : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
+            isCollise = false;
+            print("Thoat nhan vat");
             InputText.text = ("");
         }
     }
---
+++ Viral Pokemon/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public bool state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
